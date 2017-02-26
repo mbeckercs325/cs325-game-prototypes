892ccda2e55ff6b5fcf2354012ae77ba7ebb7cbc
Marc Becker <mbecke13@gmu.edu>
2017-02-26 15:03:09 -0500

    Added two new templates. Updated Digital Assignment 4 to use a gamestate template.

--- week4/js/main.js
+++ week4/js/main.js
@@ -1,17 +1,24 @@
+"use strict";
+
 window.onload = function() {
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
 
-    }
+	//	Create your Phaser game and inject it into the 'game' div.
+	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
+	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
 
-    function create() {
+	//	Add the States your game has.
+	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+	
+	// An object for shared variables, so that them main menu can show
+	// the high score if you want.
+	var shared = {};
+	
+	game.state.add( 'Boot', GameStates.makeBoot( game ) );
+	game.state.add( 'Preloader', GameStates.makePreloader( game ) );
+	game.state.add( 'MainMenu', GameStates.makeMainMenu( game, shared ) );
+	game.state.add( 'Game', GameStates.makeGame( game, shared ) );
 
-    }
-    
-    function update() {
+	//	Now start the Boot state.
+	game.state.start('Boot');
 
-    }
 };
